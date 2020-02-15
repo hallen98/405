e636c279bb093dd90aecc99182567dcd6e6629b2
David Doan <thiendoan42@gmail.com>
2020-02-15 17:00:29 -0600

    updated foreign  key missmatch

---
+++ user.sql
@@ -0,0 +1,3 @@
+use attendencemadeeasy;
+insert into user(uid,fname,lname,email,role,Student_idstudent,teacher_idteacher,password)
+values('00000001','John', 'Snow', 'JS@gmail.com', 1,0,0,'youknownothing');
