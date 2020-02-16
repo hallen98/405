6b9df03e6eb49b6883cb743011458754dc3e4642
David Doan <thiendoan42@gmail.com>
2020-02-16 15:02:55 -0600

    Update test1.sql

--- test1.sql
+++ test1.sql
@@ -11,9 +11,14 @@ SELECT user.uid,user.uid
 FROM user
 WHERE user.role = 1;
 
-/* This function pushes the user.uid to the teacher.sid if the user role is 0[student]*/
+/* This function pushes the user.uid to the teacher.sid if the user role is 0[teacher]*/
 INSERT INTO teacher(tid,User_uid)
 SELECT user.uid,user.uid
 FROM user
 WHERE user.role = 0;
 
+/*This function pushes a student to have a class where the class ID = 626*/
+INSERT INTO student_has_classes(sid,classes_idclasses,classes_teacher_idteacher)
+SELECT s.sid,c.idclasses,c.tid_class
+FROM student s, class c
+WHERE c.idclasses = 626;
