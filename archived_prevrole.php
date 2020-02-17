fab65c696adba255bfb552e72dec772c380fd670
michael-mccraryiii <33209475+michael-mccraryiii@users.noreply.github.com>
2020-02-16 18:25:00 -0600

    Add files via upload
    Updated html to php file conversion, as well as page limits document being updated.

---
+++ archived_prevrole.php
@@ -0,0 +1,55 @@
+<!--Michael McCrary-->
+<!--Archived Classes/Previous Rolls Page -->
+<!--Rough cut/working version, soft lock, whatever -->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Archived Classes/Previous Roles | Attendance Made Easy</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+ <!-- Two header section -->
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td bgcolor="#ffffff" align="center" style="padding: 8px 0 0 0;">
+                <table border="0" cellpadding="0" cellspacing="0" width="600" style="width: 600px;" class="responsive-table">
+                    <tr>
+                        <td style="padding: 20px 0 0 0;">
+                            <!-- Left section -->
+                            <table cellpadding="0" cellspacing="0" border="0" width="280" align="left" style="width: 300px;" class="responsive-table" bgcolor="#0000">
+                                <tr>
+                                    <td align="left" style="padding: 15px 0 0 20px; font-family: Helvetica, Arial, sans-serif; color: #000000; font-size: 20px;" bgcolor="#ffffff">Class Name</td>
+                                </tr>
+                            </table>
+         <!--[if (gte mso 9)|(IE)]>
+                                            </td>
+                                            <td>
+                                            <![endif]-->    
+                            <!-- Right section -->
+                            <table cellpadding="0" cellspacing="0" border="0" width="280" align="right" style="width: 300px;" class="responsive-table">
+                                <tr>
+                                    <td align="right" style="padding: 15px 20px; 0 0; font-family: Helvetica, Arial, sans-serif; color: #000000; font-size: 20px;" bgcolor="#ffffff">
+                                        Archive/History
+                                    </td>
+                                </tr>
+                            </table>
+                            <tr>
+                        <td>
+                            <!-- Body section that will implement functionality of data pulling from database -->
+                            <table width="100%" cellspacing="0" cellpadding="0" padding-top: 60px; style="padding-top: 40px;" '>
+                                <tr>
+                                    <td align="center" style="padding-bottom: 80%;font-size: 18px; font-family: Helvetica, Arial, sans-serif; border:10px solid black;"; color: #000000;" class="" >This section will contain the displayed database information containing all archived classes and previous roll records. This will be implemented in further sprints.</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table><!-- // Two header section -->
+
+</html>
