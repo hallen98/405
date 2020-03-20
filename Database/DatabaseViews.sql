d14143af538a4ec8b41591d7e37a3543ad8c75a9
David Doan <thiendoan42@gmail.com>
2020-03-20 15:44:16 -0500

    Update DatabaseViews.sql

--- Database/DatabaseViews.sql
+++ Database/DatabaseViews.sql
@@ -1,5 +1,16 @@
+# Shows the Id Class, ClassTime, Location#
 CREATE VIEW studentView AS
-	SELECT c.idclasses, c.classTime, s.location
+	SELECT c.idclasses, c.className , c.classTime, s.location
     FROM student_attended s, class c
-    WHERE s.classes_id = c.idclasses AND c.tid_class = s.classes_id;
+    WHERE s.classes_id = c.idclasses AND c.tid_class = s.classes_id AND c.archived = 0;
     
+# You should call this as a function #
+CREATE VIEW archivedView AS
+	SELECT c.className, c.classTime, c.Archived
+    FROM class c, teacher t
+    WHERE c.Archived = 1 AND t.tid = c.tid_class;
+
+CREATE VIEW currentClassesView AS
+	SELECT c.className, c.weekday, c.classTime, c.idclasses
+	FROM class c
+    WHERE c.Archived = 0
