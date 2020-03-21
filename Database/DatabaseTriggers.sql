ebdb6d85b09f79aeb881dce03243fa24d2b5fca7
David Doan <thiendoan42@gmail.com>
2020-03-21 13:30:03 -0500

    I FINALLY GOT THE TRIGGER TO WORK

--- Database/DatabaseTriggers.sql
+++ Database/DatabaseTriggers.sql
@@ -5,15 +5,17 @@ DELIMITER $$
 USE `attendencemadeeasy`$$
 CREATE
 DEFINER=`root`@`localhost`
-TRIGGER `attendencemadeeasy`.`PassWordNotHashedError`
-BEFORE UPDATE ON `attendencemadeeasy`.`usertable`
+TRIGGER attendencemadeeasy.PassWordNotHashedError
+BEFORE UPDATE ON attendencemadeeasy.usertable
 FOR EACH ROW
 BEGIN
 	DECLARE MSG VARCHAR(255);
     #Replace the 4 with Hash length size#
-	IF (CHAR_LENGTH(userType.passHash) < 4 ) THEN
+	IF (char_length(NEW.passHash) < 4 ) THEN
 		SET msg = 'Unable to set password, Not Hashed';
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
 	END IF;
 END$$
 DELIMITER ;
+
+
