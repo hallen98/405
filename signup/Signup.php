13f7533df3abc41a90a694d7f9d095078ecacffb
Hunter Allen <hqa001@latech.edu>
2020-03-24 20:58:55 -0500

    Hot fix 3

--- signup/Signup.php
+++ signup/Signup.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Create Account | Attendance Made Easy</title>
-	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="SUStyle.css" rel="stylesheet" type="text/css">
 </head>
 	<body>
 		<div class="signup">
