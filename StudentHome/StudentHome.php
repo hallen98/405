897e7c84b29eb22b72294a37f28457ac97c7d392
hallen98 <58712240+hallen98@users.noreply.github.com>
2020-03-31 19:12:49 -0500

    Update StudentHome.php

--- StudentHome/StudentHome.php
+++ StudentHome/StudentHome.php
@@ -1,31 +1,3 @@
-<?php
-$servername = "localhost";
-$username = "commit";
-$password = "TempP@ss124";
-$dbname = "attendencemadeeasy";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-//$sql = "SELECT id, firstname, lastname FROM MyGuests";
-//$result = $conn->query($sql);
-
-//if ($result->num_rows > 0) {
-    // output data of each row
-//    while($row = $result->fetch_assoc()) {
-//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//    }
-//} else {
-//    echo "0 results";
-//}
-echo "hello";
-$conn->close();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -55,6 +27,33 @@ $conn->close();
 </script>
 </head>
 <body>
+	<?php
+$servername = "localhost";
+$username = "commit";
+$password = "TempP@ss124";
+$dbname = "attendencemadeeasy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//$sql = "SELECT id, firstname, lastname FROM MyGuests";
+//$result = $conn->query($sql);
+
+//if ($result->num_rows > 0) {
+    // output data of each row
+//    while($row = $result->fetch_assoc()) {
+//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//    }
+//} else {
+//    echo "0 results";
+//}
+echo "hello";
+$conn->close();
+?>
 	
 	<div id="content">
 	
