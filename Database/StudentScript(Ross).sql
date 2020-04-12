fa28999402f4784c2206c3cc8ea01b7739d3016c
David Doan <thiendoan42@gmail.com>
2020-04-12 15:20:54 -0500

    Update StudentScript(Ross).sql

--- Database/StudentScript(Ross).sql
+++ Database/StudentScript(Ross).sql
@@ -16,3 +16,12 @@ SELECT ut1.lname, sa1.date, sa1.location
 From attendencemadeeasy.class c1, student_attended sa1, usertable ut1 
 WHERE sa1.tid_class = c1.tid_class AND sa1.idStudent = ut1.uid AND c1.tid_class = @YourTeacherIDVAR AND c1.archived = 1;
 
+#Gets the roll for a specified student for a specified class from the archived section
+use attendencemadeeasy;
+SET @YOUR_VARIABLE_FOR_THE_STUDENT_ID_HERE = 2;
+SET @YourClassIDVAR = 11111;
+SELECT c1.weekday, c1.classTime, s1.date, s1.location
+FROM attendencemadeeasy.student_attended s1, attendencemadeeasy.class c1
+WHERE s1.idStudent = @YOUR_VARIABLE_FOR_THE_STUDENT_ID_HERE AND c1.archived = 1 AND @YourClassIDVAR = c1.idclasses;
+
+
