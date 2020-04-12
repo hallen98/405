7ea3d45dbdd5070e45828eb8629f93628f47ea9d
David Doan <thiendoan42@gmail.com>
2020-04-12 15:13:11 -0500

    Update StudentScript(Ross).sql

--- Database/StudentScript(Ross).sql
+++ Database/StudentScript(Ross).sql
@@ -1,9 +1,16 @@
 use attendencemadeeasy;
 SET @YOUR_VARIABLE_FOR_THE_STUDENT_ID_HERE = 2;
-SELECT  c1.className, c1.weekday, c1.classTime, s1.date
+SELECT  c1.className, c1.weekday, c1.classTime, s1.date, s1.location
 FROM attendencemadeeasy.student_attended s1, attendencemadeeasy.class c1
-WHERE s1.classes_id = c1.idclasses AND s1.tid_class = c1.tid_class AND s1.idStudent = @YOUR_VARIABLE_FOR_THE_STUDENT_ID_HERE;
+WHERE s1.idStudent = @YOUR_VARIABLE_FOR_THE_STUDENT_ID_HERE AND c1.archived = 1;
 
 use attendencemadeeasy;
 SET @YourVAR = 1;
 SELECT c1.className, c1.classTime, c1.dataarchived, c1.weekday From attendencemadeeasy.class c1 WHERE c1.tid_class = @YourVAR AND c1.archived = 1;
+
+use attendencemadeeasy;
+SET @YourTeacherIDVAR = 1;
+SET @YourClassIDVAR = 11111;
+SELECT ut1.lname, sa1.date, sa1.location 
+From attendencemadeeasy.class c1, student_attended sa1, usertable ut1 
+WHERE sa1.tid_class = c1.tid_class AND sa1.idStudent = ut1.uid AND c1.tid_class = @YourTeacherIDVAR AND c1.archived = 1;
