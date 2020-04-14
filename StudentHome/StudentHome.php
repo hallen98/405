1ab2aff12ebc27619c9011af558cfa4a5a106e91
hallen98 <58712240+hallen98@users.noreply.github.com>
2020-04-13 21:07:21 -0500

    Update StudentHome.php

--- StudentHome/StudentHome.php
+++ StudentHome/StudentHome.php
@@ -1,35 +1,49 @@
 <?php
-/*$servername = "138.47.204.77";
+session_start();
+$userID = /*$_SESSION["userID"]*/ "2";
+$servername = "138.47.204.77";
 $username = "commit";
 $password = "TempP@ss124";
 $dbname = "attendencemadeeasy";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
-echo "Success" . PHP_EOL;
+$sql = "SELECT fname, lname FROM attendencemadeeasy.usertable WHERE uid = '$userID'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		$firstName = $row["fname"];
+		$lastName = $row["lname"];
+	}
+} else {
+	echo "0 results";
+}
 
-//$sql = "SELECT id, firstname, lastname FROM MyGuests";
-//$result = $conn->query($sql);
+$sql = "SELECT  c1.className, c1.weekday, c1.classTime, s1.location
+FROM attendencemadeeasy.student_attended s1, attendencemadeeasy.class c1
+WHERE s1.idStudent = '$userID' AND c1.archived = 0 AND c1.idclasses = s1.classes_id;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	echo "<p style='text-align: center'> some rows </p>";
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo "<p style='text-align: center'>" . $row["className"] . "</p>";
+		echo "<p style='text-align: center'>" . $row["weekday"] . "</p>";
+		echo "<p style='text-align: center'>" . $row["classTime"] . "</p>";
+	}
+} else {
+	echo "0 results";
+}
 
-//if ($result->num_rows > 0) {
-    // output data of each row
-//    while($row = $result->fetch_assoc()) {
-//        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//    }
-//} else {
-//    echo "0 results";
-//}
-echo "hello";
-$conn->close();*/
-
-$studentName = "Hunter";
-$classes = ["CSC-405-002", "CSC-406-001", "CSC-407-003"];
-
+$conn->close();
+$name = $firstName . ' ' . $lastName;
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -81,7 +95,7 @@ $classes = ["CSC-405-002", "CSC-406-001", "CSC-407-003"];
 		
 		<!-- !!!!!!!!!!!! START OF PAGE CONTENT !!!!!!!!!!!! -->
 		
-		<h1 style="text-align: center"> Welcome <?php echo $studentName; ?></h1>
+		<h1 style="text-align: center"> Welcome <?php echo $name; ?></h1>
 		
 		<center><button class="cibtn">Check In To: CSC-405-001</button></center>		<!-- Check in button -->
 		<br><br>
@@ -106,46 +120,6 @@ $classes = ["CSC-405-002", "CSC-406-001", "CSC-407-003"];
 		
 		<!-- !!!!!! EXAMPLE TABLE !!!!!! -->
 		<center>
-			<!--<table class="ctable" style="width:70%; border-collapse: collapse;">
-				<tr>
-					<th><input type="checkbox" onClick="toggle(this)"></th>
-					<th>Class</th>
-					<th>Time</th>
-					<th>Last Check In</th>
-					<th>Checked In Today</th>
-				</tr>
-				<tr class="tableRow">
-					<td class="checkBoxes"><input type="checkbox" name="checkclass" value="class1"></td>
-					<td>CSC-405-001</td>
-					<td>MWF 2:00-3:15 PM</td>
-					<td>2/2/2020</td>
-					<td><img src="check.png" alt="Check"></td>
-				</tr>
-				<tr class="tableRow">
-					<td class="checkBoxes"><input type="checkbox" name="checkclass" value="class2"></td>
-					<td>CSC-450-003</td>
-					<td>MWF 10:00-11:15 AM</td>
-					<td>2/2/2020</td>
-					<td><img src="check.png" alt="Check"></td>
-				</tr>
-				<tr class="tableRow">
-					<td class="checkBoxes"><input type="checkbox" name="checkclass" value="class3"></td>
-					<td>CSC-123-456</td>
-					<td>MWF 8:00-9:15 AM</td>
-					<td>2/1/2020</td>
-					<td><img src="x.png" alt="X"></td>
-				</tr>
-				<tr class="tableRow">
-					<td><button class="acbtn" onClick="toggleVisibility()">Add Class</button></td>
-					<td><input type="text" name="classcode" placeholder="Class Code" class="classcode" id="classcode"></td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<th><button class="delbtn">Delete Class</button></th>
-				</tr>
-				-->
 				<?php
 					echo '<table class="ctable" style="width:70%; border-collapse: collapse;">';
 					echo '<tr>';
