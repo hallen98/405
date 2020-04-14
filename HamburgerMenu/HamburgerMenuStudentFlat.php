9a1097fb931cf2cb3c4f819188f4f71208af4b8a
michael-mccraryiii <33209475+michael-mccraryiii@users.noreply.github.com>
2020-04-13 20:22:52 -0500

    Update HamburgerMenuStudentFlat.php

--- HamburgerMenu/HamburgerMenuStudentFlat.php
+++ HamburgerMenu/HamburgerMenuStudentFlat.php
@@ -1,34 +1,3 @@
-<?php
-$servername = "138.47.204.77";
-$username = "commit";
-$password = "TempP@ss124";
-$dbname = "attendancemadeeasy";
-
-//Create Connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-//Check connection 
-if ($conn->connect_error)
-{
-	die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT fname FROM attendancemadeeasy.usertable";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0)
-{
-	//output data of each row
-	while($row = $result->fetch_assoc())
-	{
-		echo "<p align='center'>".$row("fname")."</p>";
-	}
-}
-else 
-{
-	echo "0 results";
-}
-$conn->close();	
-?>
 <!DOCTYPE html>
 <html>
 <head>
