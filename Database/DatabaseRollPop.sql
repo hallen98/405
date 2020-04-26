7976eb66c328f13464ed12d5df83cef58d3c280b
David Doan <thiendoan42@gmail.com>
2020-04-26 14:53:18 -0500

    Create DatabaseRollPop.sql

---
+++ Database/DatabaseRollPop.sql
@@ -0,0 +1,14 @@
+DROP EVENT IF EXISTS studentroll;
+DELIMITER $$
+CREATE EVENT studentroll 
+ON SCHEDULE 
+EVERY 1 MINUTE  
+DO 
+BEGIN
+	SET @curr = weekday(curdate());
+	INSERT INTO `attendencemadeeasy`.`student_attended` (`idStudent`, `classes_id`, `tid_class`, `date`) 
+	SELECT shc.sid, c.idclasses,c.tid_class,CURDATE()
+	FROM attendencemadeeasy.class c, attendencemadeeasy.student_has_classes shc 
+	WHERE c.archived = 0 AND shc.classes_idclasses = c.idclasses AND((c.weekday IN ("MWF","W","M","F") AND @curr IN (0,2,4)) XOR (c.weekday IN ("TR","T","R") AND @curr IN (1,3)) XOR (c.weekday IN ("S") AND @curr IN (6)));
+END;
+DELIMITER;
