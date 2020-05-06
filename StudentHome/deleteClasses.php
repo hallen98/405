9ab021f7287bb063f2db060b989896f56ed7c926
hallen98 <58712240+hallen98@users.noreply.github.com>
2020-05-06 09:56:09 -0500

    Create deleteClasses.php

---
+++ StudentHome/deleteClasses.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$classes = $_POST["result"];
+$userID = $_SESSION["userID"];
+$classes = str_getcsv($classes[0]);
+
+//Info to connect to server
+$servername = "138.47.204.77";
+$username = "commit";
+$password = "TempP@ss124";
+$dbname = "attendencemadeeasy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+foreach($classes as $value){
+	$sql = "DELETE FROM attendencemadeeasy.student_inclass WHERE classes_idclasses = '$value' AND sid = '$userID'";
+	$result = $conn->query($sql);
+	if ($conn->query($sql) === TRUE){
+		echo "Deleted Record in DB";
+		echo "";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		echo "";
+	}	
+}
+$conn->close();
+header("Location: /StudentHome/StudentHome.php");
+exit();
+?>
