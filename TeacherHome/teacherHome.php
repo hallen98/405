7449ce89a42216cda5591c55030401bb44f9d99f
hallen98 <58712240+hallen98@users.noreply.github.com>
2020-05-06 10:01:15 -0500

    Update teacherHome.php

--- TeacherHome/teacherHome.php
+++ TeacherHome/teacherHome.php
@@ -1,3 +1,58 @@
+<?php
+//Start and get session variables
+session_start();
+$userID = $_SESSION["userID"];
+
+//Info to connect to server
+$servername = "138.47.204.77";
+$username = "commit";
+$password = "TempP@ss124";
+$dbname = "attendencemadeeasy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT fname, lname FROM attendencemadeeasy.usertable WHERE uid = '$userID'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		$firstName = $row["fname"];
+		$lastName = $row["lname"];
+	}
+} else {
+	//echo "0 results";
+}
+
+$className = [];
+$weekday = [];
+$classTime = [];
+$classes_id = [];
+
+$sql = "SELECT className, weekday, classTime, idclasses 
+FROM attendencemadeeasy.class 
+WHERE tid_class = '$userID' AND archived = 0";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		array_push($className, $row["className"]);
+		array_push($weekday, $row["weekday"]);
+		array_push($classTime, $row["classTime"]);
+		array_push($classes_id, $row["idclasses"]);
+	}
+} else {
+	//echo "0 results";
+}
+
+$conn->close();
+$name = $firstName . ' ' . $lastName;
+$date = "2020-04-15";
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,6 +78,29 @@
 			document.getElementById('content').style.marginLeft = '0'
 		}
 	}
+	function deleteClasses(i) {
+		var classIds = <?php echo json_encode($classes_id);?>;
+		var classesToDelete = [];
+		for(var j=0; j < i; j++){
+			eachCheckbox = document.getElementById('checkbox' + j);
+			if(eachCheckbox.checked){
+				classesToDelete.push(classIds[j]);
+			}
+		}
+		document.getElementById("delete").value = classesToDelete;
+	}
+	function archiveClasses(i) {
+		var classIds = <?php echo json_encode($classes_id);?>;
+		var classesToDelete = [];
+		for(var j=0; j < i; j++){
+			eachCheckbox = document.getElementById('checkbox' + j);
+			if(eachCheckbox.checked){
+				classesToDelete.push(classIds[j]);
+			}
+		}
+		console.log(classesToDelete);
+		document.getElementById("archive").value = classesToDelete;
+	}
 	
 </script>
 </head>
@@ -43,13 +121,13 @@
             <a href="/archivedClasses/ArchivedClasses.php">Archived Classes</a>
             <a href="/Settings/TeacherSettings.php">Settings</a>
 			<div class="last">
-				<a href="/login/loginpage.php">Logout</a>
+				<a href="/logout.php">Logout</a>
 			</div>
 		</div>
 		
 		<!-- !!!!!!!!!!!! START OF PAGE CONTENT !!!!!!!!!!!! -->
 		
-        <h1><center> Welcome Mike O'Neal </center></h1>
+        <h1 style="text-align: center"> Welcome <?php echo $name; ?></h1>
         <!-- SCRIPT FOR CHECK ALL BOX -->
 		<script language="JavaScript">
             function toggle(source) {
@@ -58,57 +136,60 @@
                     checkboxes[i].checked = source.checked;
                 }
             }
+		function toggleVisibility() {
+			var x = document.getElementById('classcode');
+			if (x.style.visibility === 'hidden') {
+				x.style.visibility = 'visible';
+			} else {
+				x.style.visibility = 'hidden';
+			}
+		}
             </script>
             
             <!-- !!!!!! EXAMPLE TABLE !!!!!! -->
             <center>
-                <table class="ctable" style="width:70%; border-collapse: collapse;">
+		<?php
+                echo '<table class="ctable" style="width:70%; border-collapse: collapse;">
                     <tr>
                         <th><input type="checkbox" onClick="toggle(this)"></th>
                         <th>Name</th>
                         <th>Time</th>
                         <th>Code</th>
                         <th></th>
-                        <th></th>
-                    </tr>
-                    <tr class="tableRow">
-                        <td class="checkBoxes"><input type="checkbox" name="checkclass" value="class1"></td>
-                        <td>CSC-405-001</td>
-                        <td>MWF 2:00-3:15 PM</td>
-                        <td>549826</td>
-                        <th style="text-align: right;"><button href="/roll/roll.php" class="acbtn">Roll</button></th>
-                        <th><button class="acbtn">Set Geolocation</button></th>
-                    </tr>
-                    <tr class="tableRow">
-                        <td class="checkBoxes"><input type="checkbox" name="checkclass" value="class2"></td>
-                        <td>CSC-450-003</td>
-                        <td>MWF 10:00-11:15 AM</td>
-                        <td>187205</td>
-                        <th style="text-align: right;"><button href="/roll/roll.php" class="acbtn">Roll</button></th>
-                        <th><button class="acbtn">Set Geolocation</button></th>
-                    </tr>
-                    <tr class="tableRow">
-                        <td class="checkBoxes"><input type="checkbox" name="checkclass" value="class3"></td>
-                        <td>CSC-123-456</td>
-                        <td>MWF 8:00-9:15 AM</td>
-                        <td>184402</td>
-                        <th style="text-align: right;"><button href="/roll/roll.php" class="acbtn">Roll</button></th>
-                        <th><button class="acbtn">Set Geolocation</button></th>
-                    </tr>
-                    <!-- ADDS THE ADD CLASS BUTON AND TEXT INPUT TO LINE UP WITH THE FIRST TWO COLUMBS IN THE TABLE -->
-                    <tr class="tableRow">
-                        <td><button class="acbtn">Add Class</button></td>
-                        <td><input style="visibility: hidden;"type="text" name="classcode" placeholder="Class Code" id="classcode"></td>
+                    </tr>';
+			
+		for ($i=0; $i<count($className); $i++){
+		    echo '<form action="/StudentHome/redirect.php" method="post">';
+                    echo '<tr class="tableRow">
+                        <td class="checkBoxes"><input type="checkbox" id="checkbox' . $i . '" name="checkclass" value="class2"></td>
+                        <td>' . $className[($i)] .'</td>
+                        <td>' . $weekday[($i)] . ' ' . date('h:i a', strtotime($classTime[($i)])) .'</td>
+                        <td>' . $classes_id[($i)] .'</td>
+			<input type="hidden" name="classID" value="'.$classes_id[($i)].'"> </input>
+                        <th style="text-align: right; padding: 7px;"><button class="acbtn">Roll</button></th>
+                        </tr>
+			</form>';
+		}
+                    echo '<tr class="tableRow">
+			<form action="/TeacherHome/addClass.php" method="post">
+                        <td><button class="acbtn" type="submit">Add Class</button></td>
                         <td></td>
+			</form>
                         <td></td>
                         <td></td>
                         <td></td>
                     </tr>
-                    <tr>
-                        <th><button class="archBtn">Archive Class</button></th>
-                        <th style="text-align: left;"><button class="delbtn">Delete Class</button></th>
-                    </tr>
-                </table>
+                    <tr>';
+                        echo '<form method="post" action="/TeacherHome/archiveClasses.php">';
+			echo '<th><button onClick="archiveClasses('.$i.')" class="acbtn" name="unarchive">Archive</button></th>';
+			echo '<input type="hidden" name="result[]" id="archive">';
+			echo '</form>';
+                        echo '<form action="/TeacherHome/deleteClasses.php" method="post">';
+			echo '<th><button onClick="deleteClasses('.$i.')" class="delbtn">Delete</button></th>';
+			echo '<input type="hidden" name="result[]" id="delete">';
+                    echo '</tr>
+                </table>';
+		?>
             </center>
         </div>
 </body>
